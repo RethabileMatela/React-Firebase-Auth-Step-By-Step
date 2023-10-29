21aaccdf0e78f40055b283b74b502d2b49965804
RethabileMatela <matelarethabile@gmail.com>
2023-10-29 04:01:45 +0200

    s5

--- my-ts-app/src/components/Login.tsx
+++ my-ts-app/src/components/Login.tsx
@@ -9,61 +9,33 @@ type FormValues = {
 
 function Login() {
   const form = useForm<FormValues>();
-  // 7) -then associate the devtool comp with the form you are tracking using control object you can destructure from form
-  // 9) from the form object destructure a function called handleSubmit
 
   const { register, control, handleSubmit, formState } = form;
   // 19) destructure errors object, which contains individual field errors
 
-  const { errors } = formState;   
+  const { errors } = formState;
 
-  // 8) define a function that should be called when the submit button is pressed
-  // 11) onsubmit automatically receives the form  data which we can  log into the console
-  // 14) now data is of type formValues
   const onSubmit = (data: FormValues) => {
     console.log("form submitted", data);
   };
   return (
     <div>
-      {/*render count value divided by 2 because react strict mode renders components 
-      twice during development in order to detect any problems with the code and warn us about them  */}
-
-      {/* 10) listen to the form onsubmit event and assign handleSubmit as a handler
-      to handleSubmit pass onsubmit as argument 
-
-      15) add noValidate attribute to the form element , prevents default browser validation */}
       <form onSubmit={handleSubmit(onSubmit)} noValidate>
         <label htmlFor="email">EMAIL</label>
-
-        {/*          
-         16) add required field validation to the username field 
-             a) pass an object as a second argument to the register function 
-             b) specify required as the key and the error message as its value  */}
         <input
           type="text"
           id="email"
           {...register("email", { required: "email   is required" })}
         />
-        {/* 20 ) access field errors and display them below the corresponding element ?. = optional chaining  
-        optional chaining is necessary since the error mesage for a field might never exist if no validation rules fail*/}
         <p>{errors.email?.message}</p>
-        {/* 17) Pattern validation set key to pattern and value to an object  */}
-
         <label htmlFor="password">PASSWORD</label>
         <input
           type="text"
           id="password"
           {...register("password", { required: "password   is required" })}
         />
-
         <button>Submit</button>
       </form>
-      {/* 7) invoke devtools after the closing form tag 
-      -then associate the devtool comp with the form you are tracking using control object you can destructure from form 
-      - specify a control prop and assign the control object as value
-      -touched = whether the field has been interacted with 
-      -dirty = whether the field value has changed 
-      - rhf tracks forms without rerendering the component, it follows uncontrolled inputs behaviour */}
       <DevTool control={control} />
     </div>
   );
--- my-ts-app/src/components/Register.tsx
+++ my-ts-app/src/components/Register.tsx
@@ -1,44 +1,70 @@
 import React from "react";
 // 2) install and import rhf to help manage forms with ease
 import { useForm } from "react-hook-form";
-// 6) to verify that our form is beeing tracked install and import devtool component
+// to verify that our form is beeing tracked install and import devtool component
 import { DevTool } from "@hookform/devtools";
 
-// 3) define the type of form data being sumitted by creating the form value type
-type FormValues = {
+// 3) define the type of form data being sumitted by creating the user profile object
+type userProfile = {
   email: string;
   password: string;
   confirmPassword: string;
 };
 
 function Register() {
-  // 4) link our form to useForm hook , useForm hook returns an object we call form
-  // Add UseFormValues type when invoking useForm  hook so that the UseFormValues match our submitted form values
-  const form = useForm<FormValues>();
+  // 4) link our form to useForm hook , useForm hook returns an object we call submittedFormValues
+  // Add userProfile type when invoking useForm  hook so that the userProfile match our submitted form values
+  const submittedFormValues = useForm<userProfile>();
 
-  // 3)  rhf provides Register method to help manage and register form state, that can be accessed on earlier created  form object,
-  //lets destructure it
-  const { register, control, handleSubmit, formState } = form;
+  // 5) create submittedFormValue object to destructure form values register, control, handleSubmit and formState
+  // register method from rhf registers and manages form values
+  // control object links devtool component to the form 
+  // handleSubmit is a react form onSubmit handler
+  const { register, control, handleSubmit, formState } = submittedFormValues;
 
+  // 19) destructure errors object, which contains individual field errors
+  const { errors } = formState;
+
+  // 8) define a function that should be called when the submit button is pressed
+  //  onsubmit automatically receives the form  data which we can  log into the console
+  //  now data is of type formValues
+
+  const onSubmit = (data: userProfile) => {
+    console.log("form submitted", data);
+  };
   return (
     <div>
       {/* 1) create form with labels and form fields */}
-      <form>
+      {/*render count value divided by 2 because react strict mode renders components 
+      twice during development in order to detect any problems with the code and warn us about them  */}
+
+      {/* 10) listen to the form onsubmit event and assign handleSubmit as a handler
+      to handleSubmit pass onsubmit as argument 
+
+      15) add noValidate attribute to the form element , prevents default browser validation */}
+      <form onSubmit={handleSubmit(onSubmit)} noValidate>
         <label htmlFor="email">EMAIL</label>
         <input
           type="text"
           id="email"
           // 5) use register method to allow rhf to start tracking the state of form controls by adding props
           // on input tags eg email = {email}, simplify by spreading the register method on the form control
+          // 16) add required field validation to the email field 
+          //  a) pass an object as a second argument to the register function 
+          //  b) specify required as the key and the error message as its value
           {...register("email", { required: "email   is required" })}
         />
-
+        {/* 20 ) access field errors and display them below the corresponding element ?. = optional chaining  
+        optional chaining is necessary since the error mesage for a field might never exist if no validation rules fail*/}
+        <p>{errors.email?.message}</p>
+        {/* 17) Pattern validation set key to pattern and value to an object  */}
         <label htmlFor="password">PASSWORD</label>
         <input
           type="text"
           id="password"
           {...register("password", { required: "password   is required" })}
         />
+        <p>{errors.password?.message}</p>
 
         <label htmlFor="confirmPassword">CONFIRM PASSWORD</label>
         <input
@@ -48,6 +74,8 @@ function Register() {
             required: "confirmPassword   is required",
           })}
         />
+        <p>{errors.confirmPassword?.message}</p>
+
         <button>Submit</button>
       </form>
       {/* 7) invoke devtool after closing form tag
